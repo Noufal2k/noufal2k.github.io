05099cdda9baeee3de38ecdf8f54ebe4b40bfe33
Noufal2k <kknoufal@gmail.com>
2019-07-20 12:33:08 +0300

    Updates

---
+++ precache-manifest.047539c46cf995cba4b62ee52127bd3b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "643c077879c78727939a3ecf01c4bd9e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb55ffaed737e77fc72a",
+    "url": "/static/css/main.1de97d9b.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "bb55ffaed737e77fc72a",
+    "url": "/static/js/main.1b82d09e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
